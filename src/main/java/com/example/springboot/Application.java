d6b685f048df5e7aba1fd03b4c2d1ab34ace0010
Ramakrishna <r.satti@accenture.com>
2021-04-02 09:18:23 +0530

    commandline interface

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -1,24 +1,23 @@
-package com.example.springboot;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import com.example.springboot.services.HelloService;
-
-@SpringBootApplication
-public class Application {
-	
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-		
-		String str = context.getEnvironment().getProperty("MY_SECRET");
-		System.out.println("=>>>> " + str);
-		
-		HelloService helloService = new HelloService();
-		helloService.index();
-		
-		System.exit(SpringApplication.exit(context, () -> 0));
-	}
-
-}
+/*
+ * package com.example.springboot;
+ * 
+ * import org.springframework.boot.SpringApplication; import
+ * org.springframework.boot.autoconfigure.SpringBootApplication; import
+ * org.springframework.context.ConfigurableApplicationContext;
+ * 
+ * import com.example.springboot.services.HelloService;
+ * 
+ * @SpringBootApplication public class Application {
+ * 
+ * public static void main(String[] args) { ConfigurableApplicationContext
+ * context = SpringApplication.run(Application.class, args);
+ * 
+ * String str = context.getEnvironment().getProperty("MY_SECRET");
+ * System.out.println("=>>>> " + str);
+ * 
+ * HelloService helloService = new HelloService(); helloService.index();
+ * 
+ * System.exit(SpringApplication.exit(context, () -> 0)); }
+ * 
+ * }
+ */
